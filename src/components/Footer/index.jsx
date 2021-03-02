c07677cefd87f858759548d99d470b65367c7c6e
waiting-life <70050556+waiting-life@users.noreply.github.com>
2021-03-02 16:53:05 +0800

    修改

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -16,10 +16,13 @@ export default function Footer() {
     history.push('/inCompleted')
   }
 
+  // 定义变量保存接收到的todos
   const [todos, setTodos] = useState([])
+  // Footer组件接收到从List组件发布的消息
   PubSub.subscribe('todos', (_, data) => {
     setTodos(data)
   })
+  // 获取到数据后，分别获取到全部、已完成、未完成数据的条数
   const len = todos.length
   const completedTodos = todos.filter(todo => {
     return todo.done
@@ -27,18 +30,20 @@ export default function Footer() {
   const completedLen = completedTodos.length
   const inCompletedLen = len - completedLen
 
+  // 删除已完成的数据项
   function deleteCompleted() {
+    // 过滤出已完成数据
     const newTodos = todos.filter((todo) => {
       return !todo.done
     })
-    console.log(newTodos)
+    // 发布给List组件，并且把删除了已完成得到的新数据带给List组件
     PubSub.publish('newTodos', newTodos)
   }
   return (
     <div className="footer-container">
-      <button onClick={ showAll }>全部 { len } 条</button>
-      <button onClick={showCompleted}>已完成 { completedLen } 条</button>
-      <button onClick={showIncompleted}>未完成 { inCompletedLen } 条</button>
+      <button onClick={showAll}>全部 <span>{ len }</span> 条</button>
+      <button onClick={showCompleted}>已完成 <span>{ completedLen }</span> 条</button>
+      <button onClick={showIncompleted}>未完成 <span>{ inCompletedLen }</span> 条</button>
       <button onClick={ deleteCompleted }>删除已完成</button>
     </div>
   )
